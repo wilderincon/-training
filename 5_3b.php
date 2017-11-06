de07c5182601371f7960491be17b5baf38757475
wilderincon <wilder.rincon@bitsamericas.com>
2017-11-06 08:10:24 -0500

    Exercise while loop change

--- 5_3b.php
+++ 5_3b.php
@@ -1,28 +1,20 @@
 <?php
 
 $numeros = 0;
-$terminar = 11;
+$terminar = 100;
 
-while ( $numeros <= $terminar) {
-	$final = $numeros + 1;
-	//echo $final;
-	# code...
-	$par = $numeros % 2;
-	if ($par == 1){
-
-
-		echo $numeros;
-
-     }
-     elseif($final !== $terminar ){
-     	//echo "entra if 1";
-       if($numeros !== 0){
-       	//echo "entra al if 2</br>";
-       	if($final <= $terminar){
-       		echo ",";
-       	}
-       }
-     }
-
-	$numeros++;
+while ($numeros <= $terminar) {
+    $par = $numeros % 2;
+    if ($par == 1) {
+        if ($numeros !== 0) {
+            if ($numeros !== 1 && $numeros >= 0) {
+                echo ",";
+            }
+        }
+        echo $numeros;
+    }
+    if ($numeros == $terminar) {
+        break;
+    }
+    $numeros++;
 }

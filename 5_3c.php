76445a2a4dedd3a3e31a4618b7785c7f6d38026c
wilderincon <wilder.rincon@bitsamericas.com>
2017-11-08 16:04:35 -0500

    Add New exercise about character count

---
+++ 5_3c.php
@@ -0,0 +1,31 @@
+<?php
+
+$cadena1 = "abcd1145415545545efgh";
+$cadena2 = "abcdefghijklmnopqrstu";
+
+$count1 = strlen($cadena1);
+$count2 = strlen ($cadena2);
+
+if ($count1	> $count2){
+	$difCaracteres = $count1 - $count2;
+	$message  =  " La cadena1 es mayor que la cadena2 en ".$difCaracteres." caracteres";
+}elseif ($count2 > $count1) {
+    $difCaracteres = $count2 - $count1;
+    $message	= "La cadena1 es menor que la cadena2 en ".$difCaracteres." caracteres";
+}else{
+	$message = "Las cadenas son iguales y tienen ".$count1." caracteres";
+}
+
+echo $message;
+
+
+
+
+
+
+
+
+
+
+
+

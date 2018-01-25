ea2c7ab29ec6318553c9e4fbf0dbb7ca6f5dabb7
wilderincon <wilder.rincon@bitsamericas.com>
2018-01-25 15:28:03 -0500

    Change/ code formattin3

--- 5_3c.php
+++ 5_3c.php
@@ -4,16 +4,16 @@ $cadena1 = "abcd1145415545545efgh";
 $cadena2 = "abcdefghijklmnopqrstu";
 
 $count1 = strlen($cadena1);
-$count2 = strlen ($cadena2);
+$count2 = strlen($cadena2);
 
-if ($count1	> $count2){
-	$difCaracteres = $count1 - $count2;
-	$message  =  " La cadena1 es mayor que la cadena2 en ".$difCaracteres." caracteres";
-}elseif ($count2 > $count1) {
+if ($count1 > $count2) {
+    $difCaracteres = $count1 - $count2;
+    $message = " La cadena1 es mayor que la cadena2 en " . $difCaracteres . " caracteres";
+} elseif ($count2 > $count1) {
     $difCaracteres = $count2 - $count1;
-    $message	= "La cadena1 es menor que la cadena2 en ".$difCaracteres." caracteres";
-}else{
-	$message = "Las cadenas son iguales y tienen ".$count1." caracteres";
+    $message = "La cadena1 es menor que la cadena2 en " . $difCaracteres . " caracteres";
+} else {
+    $message = "Las cadenas son iguales y tienen " . $count1 . " caracteres";
 }
 
 echo $message;

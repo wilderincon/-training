8260425ddd01fbbc6466ed83c07079a728840737
wilderincon <wilder.rincon@bitsamericas.com>
2018-01-25 15:27:16 -0500

    Change/ code formatting2

--- 5_3a.php
+++ 5_3a.php
@@ -46,42 +46,46 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        <style>
-            th{
-                border: solid 1px black;
-                font-weight: 400;
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <style>
+        th {
+            border: solid 1px black;
+            font-weight: 400;
 
-            }
-            table{
-                border: solid 1px black;
-            }
-            .title-head{
-                background: blue;
-            }
-            tr{
-                background: #a5b7ce;
-            }
-            .title-head th{
-                font-weight: 700;
-            }
-        </style>
-    </head>
-    <body>
-        <table>
-            <tr class="title-head">
-                <th>Identificador</th>
-                <th>Título</th>
-                <th>Director</th>
-                <th>Año</th>
-                <th>Duración</th>
-            </tr>
-            <?php
-            for ($i = 0; $i < count($pelicula); $i++) {
+        }
 
-                echo '<tr>
+        table {
+            border: solid 1px black;
+        }
+
+        .title-head {
+            background: blue;
+        }
+
+        tr {
+            background: #a5b7ce;
+        }
+
+        .title-head th {
+            font-weight: 700;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <tr class="title-head">
+        <th>Identificador</th>
+        <th>Título</th>
+        <th>Director</th>
+        <th>Año</th>
+        <th>Duración</th>
+    </tr>
+    <?php
+    for ($i = 0; $i < count($pelicula); $i++) {
+
+        echo '<tr>
                 <th>' . $pelicula[$i]["Identificador"] . '</th>
                 <th>' . $pelicula[$i]["Titulo"] . '</th>
                 <th>' . $pelicula[$i]["Director"] . '</th>
@@ -89,8 +93,8 @@ and open the template in the editor.
                 <th>' . $pelicula[$i]["Duracion"] . '</th>
             </tr>
             ';
-            }
-            ?>
-        </table>
-    </body>
+    }
+    ?>
+</table>
+</body>
 </html>
